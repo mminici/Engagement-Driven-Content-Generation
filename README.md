77eb0c2770c9e39bad5399aa36fd47325462715a
federicocinus <federico.cinus@gmail.com>
2024-03-14 23:27:29 +0100

    Setting environment for reproducibility

---
+++ README.md
@@ -0,0 +1,8 @@
+1. Install the environment with:
+conda env create -f environment_core.yml
+
+2. Activate the environment:
+conda activate socLLM
+
+3. Install additional library running
+sh install_packages.sh
---
+++ install_packages.sh
@@ -0,0 +1,7 @@
+conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia -y
+conda install conda-forge::transformers -y
+pip install trl
+conda install jupyter -y
+conda install -c conda-forge jupyterlab -y
+pip install peft
+conda install conda-forge::wandb -y
